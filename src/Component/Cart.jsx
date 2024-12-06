6ff17df5f5e62db13fcde876cafb521723d28d69
Enola8847 <ice9393th@gmail.com>
2024-12-06 11:49:14 +0700

    เพิ่มสีใน dialog

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -9,6 +9,8 @@ function Cart() {
   const { Cartcount, SetCartcount } = useContext(AppContext);
   const dialog = useRef();
   const [image, SetImage] = useState();
+  const [Name, SetName] = useState();
+  const [Price, SetPrice] = useState();
 
   const CloseModal = () => {
     dialog.current.close();
@@ -45,6 +47,8 @@ function Cart() {
   const Modal = (item) => {
     dialog.current.showModal();
     SetImage(item.image);
+    SetName(item.title);
+    SetPrice(item.price);
   };
   return (
     <>
@@ -99,21 +103,37 @@ function Cart() {
             <dialog
               ref={dialog}
               onClick={Clickclose}
-              className=" w-[500px] h-[250px] p-3 rounded-md"
+              className=" w-[750px] h-[300px] p-3 rounded-md  "
             >
-              <form action="" onSubmit={AddCartForm} className=" grid">
-                <h2>Name :</h2>
-                <img
-                  src={image}
-                  className=" w-[125px] h-[150px] border-2"
-                ></img>
-                <div className=" text-end ">
-                  <button
-                    className=" bg-blue-300 p-2 font-semibold"
-                    onClick={AddCart}
-                  >
-                    Add To Cart
-                  </button>
+              <form
+                action=""
+                onSubmit={AddCartForm}
+                className=" items-center flex"
+              >
+                <div>
+                  <img
+                    src={image}
+                    className=" w-[250px] h-[250px] border-2 object-fill rounded-md"
+                  ></img>
+                </div>
+                <div className="p-2 border-black border-4 w-[500px] m-4 grid rounded-md">
+                  <div className=" p-2">
+                    <header className=" font-extralight text-3xl">
+                      {Name}
+                    </header>
+                    <p className=" font-semibold text-2xl mt-2 text-end">
+                      Price : <span className=" text-red-500">{Price} </span>
+                      Bath
+                    </p>
+                  </div>
+                  <div className=" text-end mt-2 text-xl">
+                    <button
+                      className=" bg-blue-300 p-2 font-semibold hover:bg-blue-500 rounded-sm"
+                      onClick={AddCart}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
                 </div>
               </form>
             </dialog>
